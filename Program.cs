f29b49eeb6393d7b410f0e381dc4cdcf758422f5
Piotr Ludwiczuk <piotrosz@gmail.com>
2016-06-23 16:34:41 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Numerics;
+using Xunit;
+using FluentAssertions;
+
+namespace CSharp6Samples
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int x = 10;
+            Console.WriteLine($"Hello World! {x}");
+
+            var cplx = new Complex(1, 2);
+            Console.WriteLine($"Img: {cplx.Imaginary} Re: {cplx.Real} Phase: {cplx.Magnitude}");
+            Console.WriteLine(cplx);         
+        }
+    }
+
+    public class Class1
+    {
+        [Fact]
+        public void PassingTest()
+        {
+            5.Should().Be(2 + 2);;
+        }
+    }
+}
