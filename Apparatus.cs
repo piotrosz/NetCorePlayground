5316a22dd537a7c772dac5fa86af3e0daf2d4150
Piotr Ludwiczuk <piotrosz@gmail.com>
2016-06-24 16:37:09 +0200

    Flip apparatus

--- Apparatus.cs
+++ Apparatus.cs
@@ -2,9 +2,27 @@ namespace Quantum
 {
     public class Apparatus
     {
+        private int rotation;
+
         public SpinState Measure(Spin spin)
         {
-            return spin.SpinState;
+            if(rotation == 0)
+            {
+                return spin.SpinState;
+            }
+            if(rotation == 180)
+            {
+                return spin.SpinState == SpinState.Up ?
+                        SpinState.Down :
+                        SpinState.Up;
+            }  
+
+            return spin.SpinState;     
+        }
+
+        public void Rotate(int degrees)
+        {
+            rotation = (rotation + degrees) % 360;
         }
     }
 }
--- SpinMeasurementTests.cs
+++ SpinMeasurementTests.cs
@@ -13,7 +13,7 @@ namespace Quantum
         }
 
         [Fact]
-        public void given_spin_in_state_up_when_spin_measured_then_the_measurement_result_is_up()
+        public void given_spin_in_state_up_when_spin_measured_then_result_is_up()
         {
             var spin = new Spin(SpinState.Up);
             SpinState measurementResult = apparatus.Measure(spin);
@@ -21,12 +21,25 @@ namespace Quantum
         }
 
         [Fact]
-        public void given_spin_in_state_up_when_spin_measured_twice_then_the_measurement_result_is_up()
+        public void given_spin_in_state_up_when_spin_measured_twice_then_result_is_up()
         {
             var spin = new Spin(SpinState.Up);
             apparatus.Measure(spin);
             SpinState measurementResult = apparatus.Measure(spin);            
             measurementResult.Should().Be(SpinState.Up);
+        }
+
+        [Fact]
+        public void given_spin_in_state_up_when_spin_measured_and_apparatus_rotated_then_result_is_up()
+        {
+            var spin = new Spin(SpinState.Up);
+
+            var measurement1 = apparatus.Measure(spin);
+            apparatus.Rotate(180);
+            var measurement2 = apparatus.Measure(spin);
+
+            measurement1.Should().Be(SpinState.Up);
+            measurement2.Should().Be(SpinState.Down);
         }
     }
 }
