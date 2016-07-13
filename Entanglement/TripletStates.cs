16f7a0efad850cf6c5dfaf1865ab7cb0af4235da
Piotr Ludwiczuk <piotrosz@gmail.com>
2016-07-14 00:28:31 +0200

    Add TripletStates class

---
+++ Entanglement/TripletStates.cs
@@ -0,0 +1,10 @@
+namespace Quantum
+{
+    public static class TripletStates
+    {
+        public static readonly Matrix T1;
+        public static readonly Matrix T2;
+
+        public static readonly Matrix T3;
+    }
+}
