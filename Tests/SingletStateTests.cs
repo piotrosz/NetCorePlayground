44a3439f00010ea2d19f6ce7e3b84d04ce52a255
Piotr Ludwiczuk <piotrosz@gmail.com>
2016-07-14 00:26:26 +0200

    One more test for singlet state

--- Tests/SingletStateTests.cs
+++ Tests/SingletStateTests.cs
@@ -42,5 +42,16 @@ namespace Quantum
                     Tensor.Product(PauliMatrices.sigma_y, Matrix.GetIdentityMatrix(2, 2)))
             .Should().Be(Complex.Zero);
         }
+
+        // |sing> is a eigen vector of observable tau_z*sigma_z having eigen value -1.
+        // Alice and Bob always have opposite results of measurement: -1, 1 or 1, -1.
+        // * - tensor product
+        [Fact]
+        public void tau_z_sigma_z_acting_on_singlet_gives_minus_singlet()
+        {
+            var result = Tensor.Product(PauliMatrices.sigma_z, PauliMatrices.sigma_z) * SingletState.GetMatrix();
+
+            (result == -1 * SingletState.GetMatrix()).Should().BeTrue();
+        }
     }
 }
