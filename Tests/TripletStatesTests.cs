f3e91d363b0dd5adf331736aa7431a56712152a5
Piotr Ludwiczuk <piotrosz@gmail.com>
2016-07-14 20:19:12 +0200

    Average values of sigma sigma xyz operators for triplet states

--- Tests/TripletStatesTests.cs
+++ Tests/TripletStatesTests.cs
@@ -14,20 +14,24 @@ namespace Quantum
                 .Sandwich(
                     Tensor.Product(PauliMatrices.sigma_z, Matrix.GetIdentityMatrix(2)));
 
-            Console.WriteLine(result);
+            result.Should().Be(Complex.Zero);
         }
 
+        // T1
         [Fact]
         public void given_t1_state_sigma_z_tau_z()
         {
             var result = Tensor.Product(PauliMatrices.sigma_z, PauliMatrices.sigma_z) * TripletStates.T1();
             //Console.WriteLine(result);
+
+            (result == -1 * TripletStates.T1()).Should().BeTrue();
         }
 
         [Fact]
         public void given_t1_state_sigma_y_tau_y()
         {
             var result = Tensor.Product(PauliMatrices.sigma_y, PauliMatrices.sigma_y) * TripletStates.T1();
+            (result == TripletStates.T1()).Should().BeTrue();
             //Console.WriteLine(result);
         }
 
@@ -35,7 +39,57 @@ namespace Quantum
         public void given_t1_state_sigma_x_tau_x()
         {
             var result = Tensor.Product(PauliMatrices.sigma_x, PauliMatrices.sigma_x) * TripletStates.T1();
+            (result == TripletStates.T1()).Should().BeTrue();
             //Console.WriteLine(result);
+        }
+
+        // T2
+        [Fact]
+        public void given_t2_state_sigma_z_tau_z()
+        {
+            var result = Tensor.Product(PauliMatrices.sigma_z, PauliMatrices.sigma_z) * TripletStates.T2();
+            //Console.WriteLine(result);
+            //Console.WriteLine(TripletStates.T2());
+            (result == TripletStates.T2()).Should().BeTrue();
+        }
+
+        [Fact]
+        public void given_t2_state_sigma_y_tau_y()
+        {
+            var result = Tensor.Product(PauliMatrices.sigma_y, PauliMatrices.sigma_y) * TripletStates.T2();
+            //Console.WriteLine(result);
+            
+            (result == -1 * TripletStates.T2()).Should().BeTrue();
+        }
+
+        [Fact]
+        public void given_t2_state_sigma_x_tau_x()
+        {
+            var result = Tensor.Product(PauliMatrices.sigma_x, PauliMatrices.sigma_x) * TripletStates.T2();
+            //Console.WriteLine(result);
+            (result == TripletStates.T2()).Should().BeTrue();
+        }
+
+        // T3
+        [Fact]
+        public void given_t3_state_sigma_z_tau_z()
+        {
+            var result = Tensor.Product(PauliMatrices.sigma_z, PauliMatrices.sigma_z) * TripletStates.T3();
+            (result == TripletStates.T3()).Should().BeTrue();
+        }
+
+        [Fact]
+        public void given_t3_state_sigma_y_tau_y()
+        {
+            var result = Tensor.Product(PauliMatrices.sigma_y, PauliMatrices.sigma_y) * TripletStates.T3();
+            (result == TripletStates.T3()).Should().BeTrue();
+        }
+
+        [Fact]
+        public void given_t3_state_sigma_x_tau_x()
+        {
+            var result = Tensor.Product(PauliMatrices.sigma_x, PauliMatrices.sigma_x) * TripletStates.T3();
+            (result == -1 * TripletStates.T3()).Should().BeTrue();
         }
     }
 }
