c8ec41094aad240b6c8d5d9519827c27ebeb196d
Piotr Ludwiczuk <piotrosz@gmail.com>
2016-07-14 15:33:32 +0200

    triplet

--- Tests/TripletStatesTests.cs
+++ Tests/TripletStatesTests.cs
@@ -8,25 +8,34 @@ namespace Quantum
     public class TripletStatesTests
     {
         [Fact]
-        public void sigma_z_tau_z()
+        public void given_t1_state_then_avarage_of_sigma_z_()
         {
-            var result = Tensor.Product(PauliMatrices.sigma_z, PauliMatrices.sigma_z) * TripletStates.T1();
-
-            Console.WriteLine(TripletStates.T1());
+            var result = TripletStates.T1()
+                .Sandwich(
+                    Tensor.Product(PauliMatrices.sigma_z, Matrix.GetIdentityMatrix(2)));
 
             Console.WriteLine(result);
         }
 
         [Fact]
-        public void sigma_y_tau_y()
+        public void given_t1_state_sigma_z_tau_z()
         {
-            
+            var result = Tensor.Product(PauliMatrices.sigma_z, PauliMatrices.sigma_z) * TripletStates.T1();
+            //Console.WriteLine(result);
         }
 
         [Fact]
-        public void sigma_x_tau_x()
+        public void given_t1_state_sigma_y_tau_y()
         {
-            
+            var result = Tensor.Product(PauliMatrices.sigma_y, PauliMatrices.sigma_y) * TripletStates.T1();
+            //Console.WriteLine(result);
+        }
+
+        [Fact]
+        public void given_t1_state_sigma_x_tau_x()
+        {
+            var result = Tensor.Product(PauliMatrices.sigma_x, PauliMatrices.sigma_x) * TripletStates.T1();
+            //Console.WriteLine(result);
         }
     }
 }
